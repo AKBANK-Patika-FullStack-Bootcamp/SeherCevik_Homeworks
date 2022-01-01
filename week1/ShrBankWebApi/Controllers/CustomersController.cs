a665cfd68cbc1db8fecb7408d5b3d9b0242b62b8
SeherCevik <78930411+SeherCevik@users.noreply.github.com>
2022-01-01 22:47:31 +0300

    CustomerController Edited and PostmanScreenshots added

--- week1/ShrBankWebApi/Controllers/CustomersController.cs
+++ week1/ShrBankWebApi/Controllers/CustomersController.cs
@@ -8,14 +8,61 @@ namespace ShrBankWebApi.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
-        static List<Customer> CustomerList = new List<Customer>() 
-        { 
-            new Customer {  Id = 1, name = "Seher", LastName = "Cevik", password = 123, debt = 0, asset = 1000000, GSM = "5437612095" },
-            new Customer { Id = 2, name = "Tamer", LastName = "Omeroglu", password = 2322, debt = 0, asset = 1000000, GSM = "5437456132" }
+        static List<Customer> CustomerList = new List<Customer>()
+        {
+            new Customer {  Id = 1, name = "Seher", LastName = "Cevik", password = 123, debt = 0, asset = 1000000, GSM = "5457313096" },
+            new Customer { Id = 2, name = "Tamer", LastName = "Omeroglu", password = 2322, debt = 0, asset = 1000000, GSM = "5437456132" },
+            new Customer { Id = 3, name = "Yıldız", LastName = "Omeroglu", password = 6901, debt = 0, asset = 1000000, GSM = "5477417122" },
+            new Customer { Id = 4, name = "Hacer", LastName = "Cevik", password = 7219, debt = 0, asset = 1000000, GSM = "5337456132" }
          };
         Result _result = new Result();
 
-        [HttpPost]
+
+
+        [HttpGet("getall")]
+        public Result GetAllCustomer()
+        {
+            if (CustomerList.Count > 0)
+            {
+                _result.status = 1;
+                _result.Message = "Customer listesi ";
+                _result.CustomerList = CustomerList;
+
+            }
+            else
+            {
+                _result.status = 0;
+                _result.Message = "Kayıt bulunamadı.";
+            }
+            return _result;
+        }
+
+        [HttpGet("{Id}")]
+        public Result GetCustomer(int Id)
+        {
+
+            // List<Customer> customer = new List<Customer>();
+
+            //buradaki firstordefault u silip where e çektim ve sonuna tolist() ekledim çünkü bizim result nesnemizdeki data  property miz list tipinde. firstordefault ifadesi bize bir liste dönmediği için where ifade ile sorgulayıp tolist ifadesi ile de sorgusonucunu listeye dönüştürdüm.
+            var resultCustomer = CustomerList.Where(x => x.Id == Id).ToList();
+
+            if (resultCustomer.Count > 0)
+            {
+
+                _result.status = 1;
+                _result.Message = "Kayıt Bulundu";
+                _result.CustomerList = resultCustomer;
+            }
+            else
+            {
+                _result.status = 1;
+                _result.Message = "Kayıt Bulunamadı";
+            }
+
+            return _result;
+        }
+
+        [HttpPost("add")]
         public Result AddCustomer(Customer customer)
         {
 
@@ -38,30 +85,6 @@ namespace ShrBankWebApi.Controllers
             return _result;
         }
 
-        [HttpGet("{Id}")]
-        public Result GetCustomer(int Id)
-        {
-
-           // List<Customer> customer = new List<Customer>();
-
-          //buradaki firstordefault u silip where e çektim ve sonuna tolist() ekledim çünkü bizim result nesnemizdeki data  property miz list tipinde. firstordefault ifadesi bize bir liste dönmediği için where ifade ile sorgulayıp tolist ifadesi ile de sorgusonucunu listeye dönüştürdüm.
-            var resultCustomer = CustomerList.Where(x => x.Id == Id).ToList();
-
-            if (resultCustomer.Count > 0)
-            {
-
-                _result.status = 1;
-                _result.Message = "Kayıt Bulundu";
-                _result.CustomerList = resultCustomer;
-            }
-            else
-            {
-                _result.status = 1;
-                _result.Message = "Kayıt Bulunamadı";
-            }
-
-            return _result;
-        }
 
         [HttpPut("{Id}")]
         //birden çok dönüş türü varsa IActionResult uygulanabilir.
@@ -85,7 +108,23 @@ namespace ShrBankWebApi.Controllers
             return _result;
         }
 
-
-        
+        [HttpDelete("{Id}")]
+        public Result Delete(int Id)
+        {
+            //kayıt var mı yok mu diye bakılır
+            var customer = CustomerList.FirstOrDefault(k => k.Id == Id);
+            if (customer is not null)
+            {
+                CustomerList.Remove(customer);
+                _result.status = 1;
+                _result.Message = "kayıt başarıyla silindi";
+            }
+            else
+            {
+                _result.status = 0;
+                _result.Message = "Silmek istediğiniz kayıt bulunamadı";
+            }
+            return _result;
+        }
     }
 }
